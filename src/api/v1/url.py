a9a5d5e76a284c5cc96c876952df3160d5e97562
zrow <mrzrow@yandex.ru>
2025-03-26 09:29:39 +0300

    post method returns shor url

--- src/api/v1/url.py
+++ src/api/v1/url.py
@@ -1,19 +1,26 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+from pydantic import HttpUrl
 
 from src.domain.entities.url import Url
-from src.domain.repositories.dto.url import CreateUrlDTO, GetByIdUrlDTO, GetByShortUrlDTO
+from src.domain.repositories.dto.url import CreateUrlDTO, GetByIdUrlDTO, GetByShortUrlDTO, CreateUrlWithBaseDTO, \
+    GetShortUrlDTO
 from src.domain.services.url import UrlService
 from src.infrastructure.depends import get_url_service
 
 router = APIRouter(tags=['URL'], prefix='/url')
 
 
-@router.post('/')
+@router.post('/', response_model=GetShortUrlDTO)
 async def create_url(
         url_in: CreateUrlDTO,
+        request: Request,
         service: UrlService = Depends(get_url_service)
 ):
-    return await service.create(url_in)
+    base_url = HttpUrl(str(request.url))
+    result = await service.create(url_in)
+    return GetShortUrlDTO(
+        short_url=HttpUrl(f'{base_url}{result.short_url}')
+    )
 
 
 @router.get('/id/{url_id}', response_model=Url)
